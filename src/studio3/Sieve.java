8bfd02b3c5c06d9cdab4f32c7ebdf048566fd197
ruohanwang <ruohanwang@localhost>
2022-09-22 12:46:50 -0500

    Ruohan Wang, Russell Zeng, 9.22

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,30 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("What is the input number?");
+		int n = in.nextInt();
+		int[] anArray = new int[n-1];
+		for (int i = 0; i < anArray.length; i++)
+		{
+			if (anArray[i] == 0)
+			{
+				anArray[i] = i + 2;
+				System.out.println(anArray[i]);
+				for (int j = i + (i + 2); j < anArray.length; j = i + 2)
+				{
+					anArray[j] = 1;
+					System.out.println((j + 2));
+				}
+			}
+		}
+		
+		
+	}
+
+}
